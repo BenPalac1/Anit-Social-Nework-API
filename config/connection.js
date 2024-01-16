de7e4d21e0ada06e11914aa7163547b5ddb03348
BenPalac1 <ben.palacpac@gmail.com>
2024-01-16 16:54:56 -0500

    upadted connection, designated space for my mongo db connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,8 @@
 const { connect, connection } = require('mongoose');
 
 
-connect(//put mongo connection in here`); 
+connect(); //put mongo connection here
 
 // const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
-
-connect(connectionString);
 
 module.exports = connection;
