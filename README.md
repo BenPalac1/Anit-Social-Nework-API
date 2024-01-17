2979f5088ce67377a31fabfb5e617464af8878e8
BenPalac1 <ben.palacpac@gmail.com>
2024-01-17 18:05:09 -0500

    updated User model as per guidelines from mod challenge

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Anit-Social-Nework-API
+
+
+Resources used:
+
+modules from class, 
+
+especially the mini project from module 18
+
+stack overflow
---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v) { //used a modified version of the regex from module 17 gist, included Capital letter params
+                    return /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(v);
+                },
+                message: "Please enter a valid email"
+            },
+        },
+
+        thoughts:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+
+        friends:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+//use userSchema to make User model
+const User = model('User', userSchema);
+
+module.exports = User
